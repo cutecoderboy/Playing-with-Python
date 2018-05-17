eb99e525faa93994316354a16bcb999902b5dd5d
cutecoderboy <34506526+cutecoderboy@users.noreply.github.com>
2018-05-17 10:30:15 +0200

    Add files via upload

---
+++ textadventure.py
@@ -0,0 +1,6 @@
+""" A text adventure """
+
+print('Welcome to my Dungeon Hunter 9000')
+print('Be prepared for the adventure of a time')
+input_string_name = input('Type in your name: ')
+print('We have been expecting you, '+input_string_name)
